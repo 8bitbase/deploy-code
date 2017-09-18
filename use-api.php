2386fd8cad1de7e3f9bd161614f0151eea51bd6d
Trinh Tan Dat <ttdat.email@gmail.com>
2017-09-18 14:14:00 +0700

    update source code

---
+++ use-api.php
@@ -0,0 +1,38 @@
+<?php
+    $ch = curl_init();
+    $code = 'using System;
+        using System.Collections.Generic;
+        using System.Linq;
+        using System.Text.RegularExpressions;
+
+        namespace Rextester
+        {
+            public class Program
+            {
+                public static void Main(string[] args)
+                {
+                    //Your code goes here
+                    Console.WriteLine("Hello, world!");
+                }
+            }
+        }';
+
+    $fields = array(
+        "lang" => "C#",
+	    "code" => $code,
+		"token" => "0"
+    );
+    $postvars = '';
+    foreach($fields as $key=>$value) {
+        $postvars .= $key . "=" . $value . "&";
+    }
+    $url = "http://api.8bitbase.com/deploy/execute";
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_POST, 1);                //0 for a get request
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$postvars);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT ,3);
+    curl_setopt($ch,CURLOPT_TIMEOUT, 20);
+    $response = curl_exec($ch);
+    curl_close ($ch);
+    print_r($response);

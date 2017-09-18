0196a1d761d4228fed6c15c828c9e400293bd26a
Trinh Tan Dat <ttdat.email@gmail.com>
2017-09-18 22:41:22 +0700

    Use api php v.0.0.1

--- use-api.php
+++ use-api.php
@@ -7,8 +7,8 @@
         $ch = curl_init();
         $fields = array(
             "lang" => $lang,
-    	    "code" => $code,
-    		"token" => $token
+            "code" => $code,
+            "token" => $token
         );
         $postvars = '';
         foreach($fields as $key=>$value) {
@@ -29,20 +29,3 @@
         {"error":"","message":"OK","result":"Hello, world!\r\n","stats":"Compilation time: 0,12 sec, absolute running time: 0,08 sec, cpu time: 0,08 sec, average memory usage: 13 Mb, average nr of threads: 2"}
         */
     }
-<<<<<<< HEAD
-=======
-    $url = "http://api.8bitbase.com/deploy/execute";
-    curl_setopt($ch,CURLOPT_URL,$url);
-    curl_setopt($ch,CURLOPT_POST, 1);                //0 for a get request
-    curl_setopt($ch,CURLOPT_POSTFIELDS,$postvars);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT ,3);
-    curl_setopt($ch,CURLOPT_TIMEOUT, 20);
-    $response = curl_exec($ch);
-    curl_close ($ch);
-    print_r($response);
-    // Return
-    /*
-    {"error":"","message":"OK","result":"Hello, world!\r\n","stats":"Compilation time: 0,12 sec, absolute running time: 0,08 sec, cpu time: 0,08 sec, average memory usage: 13 Mb, average nr of threads: 2"}
-    */
->>>>>>> 4c125b14fef6698d805bf7be9a60cfc80c1b0ac0
